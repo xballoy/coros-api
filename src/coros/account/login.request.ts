beefcbff544c422eaf92be8eea66138579e3b543
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-05-07 22:22:11 -0400

    build: use zod v4

--- src/coros/account/login.request.ts
+++ src/coros/account/login.request.ts
@@ -2,7 +2,7 @@ import { createHash } from 'node:crypto';
 import { URL } from 'node:url';
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { z } from 'zod';
+import { z } from 'zod/v4';
 import { BaseRequest } from '../base-request';
 import { CorosResponse } from '../common';
 import { CorosAuthenticationService } from '../coros-authentication.service';
